832e094fd1fcdbc6a887439b4158e595cfbf8ccc
Salvaji Bhuvan <21e51a6747@hitam.org>
2025-02-07 21:02:57 +0000

    modified test.js file

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,21 @@
-console.log("bhuvi")
+// console.log("bhuvi")
+
+const accountId = 183549
+let accountEmail = "bhuvi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// console.log(accountId)
+
+// accountId = 192537
+// console.log(accountId)
+accountCity = "Benguluru"
+accountEmail = "bhu@bhu.com"
+accountPassword = "54321"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
